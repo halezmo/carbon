4f0becd3f1a27b6d203160ffd8c00b27d1a06255
Paul Sturgess <paul@kyanmedia.com>
2017-08-02 13:44:43 +0100

    Re-enable the ghost row for drag and drop functionality

---
+++ src/components/drag-and-drop/with-drag-drop/__spec__.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { mount } from 'enzyme';
+import { DragDropContext } from 'react-dnd';
+import TestBackend from 'react-dnd-test-backend';
+import WithDragDrop from './with-drag-drop';
+import DraggableContext from './../draggable-context';
+
+describe('WithDragDrop', () => {
+  let wrapper, wrapper2;
+
+  const DummyComponent = () => <div>Test</div>;
+
+  describe('render', () => {
+    let connectDragSource
+    beforeEach(() => {
+      connectDragSource = (bar) => { foo: bar }
+      wrapper = mount(
+        <DraggableContext onDrag={ () => {} }>
+          <WithDragDrop>
+            <DummyComponent />
+          </WithDragDrop>
+        </DraggableContext>
+      )
+    })
+    it('clones the children with props passed through', () => {
+      const child = wrapper.find(DummyComponent)
+      expect(child.props().connectDragSource).toEqual(jasmine.any(Function))
+      expect(child.props().connectDragPreview).toEqual(jasmine.any(Function))
+    });
+  });
+
+  describe('Drag and drop', () => {
+    let backend, handlerId, targetId, beginDragContextSpy, beginDragPropSpy,
+    endDragContextSpy, endDragPropSpy, hoverContextSpy, hoverPropSpy,
+    component, component2;
+
+    const TestComponent = (props) => {
+      return(
+        props.connectDragPreview(
+          props.connectDragSource(
+            <div>foo</div>
+          )
+        )
+      )
+    }
+
+    function createWrapper(props = {}) {
+      let DnD = wrapInTestContext(WithDragDrop);
+      wrapper = mount(
+        <DnD { ...props } index={1}>
+          <TestComponent index={1} />
+        </DnD>
+      );
+
+      wrapper2 = mount(
+        <DnD { ...props } index={2}>
+          <TestComponent index={2} />
+        </DnD>
+      );
+
+      component = wrapper.find(WithDragDrop).getNode();
+      handlerId = component.getDecoratedComponentInstance().getHandlerId();
+      component2 = wrapper2.find(WithDragDrop).getNode();
+      targetId = component2.getHandlerId();
+      backend = wrapper.getNode().getManager().backend;
+    }
+
+    function wrapInTestContext(DecoratedComponent) {
+      return DragDropContext(TestBackend)(
+        class TestContextContainer extends React.Component {
+          static childContextTypes = {
+            dragAndDropBeginDrag: PropTypes.func,
+            dragAndDropEndDrag: PropTypes.func,
+            dragAndDropHover: PropTypes.func
+          }
+
+          getChildContext() {
+            return {
+              dragAndDropBeginDrag: beginDragContextSpy,
+              dragAndDropEndDrag: endDragContextSpy,
+              dragAndDropHover: hoverContextSpy
+            };
+          }
+
+          render() {
+            return <DecoratedComponent {...this.props} />;
+          }
+        }
+      );
+    }
+
+    beforeEach(() => {
+      hoverContextSpy = jasmine.createSpy().and.callThrough();
+      hoverPropSpy = jasmine.createSpy().and.callThrough();
+      beginDragContextSpy = jasmine.createSpy().and.returnValue({ index: 1 });
+      beginDragPropSpy = jasmine.createSpy().and.returnValue({ index: 1 });
+      endDragContextSpy = jasmine.createSpy();
+      endDragPropSpy = jasmine.createSpy();
+    });
+
+    describe('without custom props', () => {
+      beforeEach(() => {
+        createWrapper();
+      });
+
+      describe('beginDrag', () => {
+        it('calls the beginDrag from context', () => {
+          backend.simulateBeginDrag([handlerId]);
+          expect(beginDragPropSpy).not.toHaveBeenCalled();
+          expect(beginDragContextSpy).toHaveBeenCalled();
+        });
+      });
+
+      describe('endDrag', () => {
+        it('calls the endDrag from context', () => {
+          backend.simulateBeginDrag([handlerId]);
+          backend.simulateEndDrag([handlerId]);
+          expect(endDragPropSpy).not.toHaveBeenCalled();
+          expect(endDragContextSpy).toHaveBeenCalled();
+        });
+      });
+
+      describe('hover', () => {
+        it('calls the hover from context', () => {
+          backend.simulateBeginDrag([handlerId]);
+          backend.simulateHover([targetId]);
+          expect(hoverPropSpy).not.toHaveBeenCalled();
+          expect(hoverContextSpy).toHaveBeenCalled();
+        });
+      });
+    })
+
+    describe('with custom props', () => {
+      beforeEach(() => {
+        createWrapper({
+          hover: hoverPropSpy,
+          beginDrag: beginDragPropSpy,
+          endDrag: endDragPropSpy
+        });
+      });
+
+      describe('hover', () => {
+        it('calls the hover from props', () => {
+          backend.simulateBeginDrag([handlerId]);
+          backend.simulateHover([targetId]);
+          expect(hoverPropSpy).toHaveBeenCalled();
+          expect(hoverContextSpy).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('beginDrag', () => {
+        it('calls the beginDrag from context', () => {
+          backend.simulateBeginDrag([handlerId]);
+          expect(beginDragPropSpy).toHaveBeenCalled();
+          expect(beginDragContextSpy).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('endDrag', () => {
+        it('calls the endDrag from context', () => {
+          backend.simulateBeginDrag([handlerId]);
+          backend.simulateEndDrag([handlerId]);
+          expect(endDragPropSpy).toHaveBeenCalled();
+          expect(endDragContextSpy).not.toHaveBeenCalled();
+        });
+      });
+    })
+
+    describe('when the canDrag prop is a function that returns false', () => {
+      beforeEach(() => {
+        createWrapper({
+          canDrag: () => false
+        });
+      });
+
+      it('it does not trigger begin drag', () => {
+        backend.simulateBeginDrag([handlerId]);
+        expect(beginDragContextSpy).not.toHaveBeenCalled();
+      });
+    })
+  });
+});
